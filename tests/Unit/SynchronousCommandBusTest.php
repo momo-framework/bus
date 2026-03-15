7f115f44439aecf5c4326a6ba23eedaac47e3453
Vahe Sargsyan <w33bv.gl@gmail.com>
2026-03-15 18:56:10 +0300

    add license

--- tests/Unit/SynchronousCommandBusTest.php
+++ tests/Unit/SynchronousCommandBusTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Part of Momo Framework.
+ *
+ * @copyright Vahe Sargsyan <w33bvGL>
+ * @license   AGPL-3.0-or-later <https://www.gnu.org/licenses/agpl-3.0.html>
+ * @link      https://github.com/momo-framework
+ */
+
 declare(strict_types=1);
 
 namespace Momo\Bus\Tests\Unit;
